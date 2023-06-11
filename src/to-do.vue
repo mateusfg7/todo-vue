f6c9b6d72751e7cb18e08dc1add79892cbe01adc
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2023-06-11 00:51:56 -0300

    feat(ui): create Todo component

---
+++ src/to-do.vue
@@ -0,0 +1,24 @@
+<script lang="ts" setup>
+import { ref } from 'vue'
+import { PhTrash } from '@phosphor-icons/vue'
+
+defineProps<{ name: string; id: string; removeFn: (id: string) => void }>()
+
+const isComplete = ref(false)
+</script>
+
+<template>
+  <div class="flex items-center justify-between gap-5 text-2xl group">
+    <div class="flex items-center gap-5">
+      <input
+        type="checkbox"
+        v-model="isComplete"
+        class="border border-neutral-700 p-3 rounded focus:outline-none focus:ring-0"
+      />
+      <span :class="isComplete && 'line-through text-neutral-500'">{{ name }}</span>
+    </div>
+    <button @click="removeFn(id)" class="transition-opacity opacity-0 group-hover:opacity-100">
+      <PhTrash weight="duotone" class="text-red-700" />
+    </button>
+  </div>
+</template>
