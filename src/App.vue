c43c809d5560e30f6e95ce7d050ec48f9883943c
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2023-06-11 00:52:21 -0300

    feat(ui): create main todo logic and import todo component

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,50 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { reactive, ref } from 'vue'
+import { PhPlus } from '@phosphor-icons/vue'
+import Todo from './to-do.vue'
+
+const slugger = (text: string) =>
+  text.toLowerCase().trim().replace(' ', '-') + '-' + (Math.random() * 100).toFixed()
+
+const todoInput = ref('')
+const todoList = reactive({ todos: [{ name: 'task 1', id: slugger('task 1') }] })
+
+function addNewTodo() {
+  todoList.todos.push({ name: todoInput.value, id: slugger(todoInput.value) })
+  todoInput.value = ''
+}
+
+function removeTodo(todoId: string) {
+  todoList.todos = todoList.todos.filter((todo) => todo.id !== todoId)
+}
+</script>
 
 <template>
-  <h1>Hello Word</h1>
+  <div class="space-y-10">
+    <div class="flex items-stretch justify-between gap-3">
+      <input
+        type="text"
+        v-model="todoInput"
+        @keypress.enter="addNewTodo"
+        class="border border-neutral-700 p-2 rounded-lg w-full"
+      />
+      <button
+        @click="addNewTodo"
+        class="border border-neutral-700 text-neutral-700 p-3 rounded-lg transition-colors hover:bg-neutral-700 hover:text-neutral-100"
+      >
+        <PhPlus />
+      </button>
+    </div>
+    <div class="space-y-1">
+      <Todo
+        v-for="todo in todoList.todos"
+        :key="todo.id"
+        :id="todo.id"
+        :name="todo.name"
+        :remove-fn="removeTodo"
+      />
+    </div>
+  </div>
 </template>
 
 <style lang="postcss"></style>
